a48ab71e7921b10339e482b475a90738fb55597a
vectorg3 <diglytail@gmail.com>
2023-04-20 22:41:17 +0300

    email required

--- models/User.js
+++ models/User.js
@@ -24,6 +24,7 @@ const UserSchema = new mongoose.Schema(
         email: {
             type: String,
             unique: true,
+            required: true
         },
         passwordHash: {
             type: String,
