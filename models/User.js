f118718616c8bf7268abe6b50a262b66ea803e5b
vectorg3 <diglytail@gmail.com>
2023-04-20 18:59:58 +0300

    user model fix

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
-        secondName: {
+        lastName: {
             type: String,
             required: true,
         },
